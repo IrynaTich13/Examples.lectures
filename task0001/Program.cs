29b6cc4085fc53bd61cf1e0958cfb794a9e6911c
Iryna <tichaya.13@gmail.com>
2023-04-02 18:08:02 +0300

    Создала папку свою для разного

---
+++ task0001/Program.cs
@@ -0,0 +1,57 @@
+﻿
+//  Искала варианты решения задачи №19
+
+
+Console.Write("Введите пятизначное число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+string num = Console.ReadLine();
+int size_num = num[0] == '-'? num.Length - 1 : num.Length;
+
+Console.WriteLine(size_num ? "Да" : "Нет");
+
+// bool Palindrom(num)
+// {
+//     if
+// }
+
+
+
+//  bool g = false;
+//             int count = 0;
+//             int a = Convert.ToInt32(Console.ReadLine()); 
+//             int b = 0;
+//             if (a == 0)
+//             { g = true; }
+            
+//             {
+//                 while (a != 0)
+//                 {
+//                     b = a % 10;
+//                     a = a / 10;
+//                     count++;
+ 
+ 
+//                 }
+//             }
+//             if (g)
+//             { Console.WriteLine("Количество цифр: " + 1); }
+//             else
+//             {
+//                 Console.WriteLine("Количество цифр: " + count);
+//             }
+//             Console.ReadLine();
+
+            //    Console.WriteLine("Ведите число");
+            // int x = int.Parse(Console.ReadLine());
+            // int f = x % 10;
+            // int i = 1;
+            // do
+            // {
+            //     i++;
+            //     x /= 10;
+            // } while (x > 10);
+            // Console.WriteLine("Количество: "+i.ToString());
+            // Console.WriteLine("Первая цифра: " + x.ToString());
+            // Console.WriteLine("Последняя цифра: " + f.ToString());
+            // Console.ReadKey();
